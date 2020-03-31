ba36249a861af0d61291fbb9cb386a28770675fa
ralphramos22 <57271219+ralphramos22@users.noreply.github.com>
2020-03-31 12:37:53 -1000

    Add files via upload

---
+++ 2page/2script.js
@@ -0,0 +1,10 @@
+function z()
+  {for(c=b=j=25,r=Math.random,
+    s[i='innerHTML']='';
+    j;
+    c+b-5|c-m&&b-m?s[i]+="URFBLD"
+    [j--,c=b,b=m]+" 2'"[0|r()*3]+" ":0)
+    m=0|r()*6}z(),
+    k=onkeyup=function(x)
+    {x.which^32||k?(S=new Date,T=setInterval("a[i]=(new Date-S)/1e3"))
+    :(clearInterval(T),z());k=!k} 
